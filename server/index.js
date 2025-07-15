11941e460584338a80b610195669a51309a901b2
Parth Kalekar <parthkalekar2002@gmail.com>
2025-07-15 12:43:48 +0530

    added cors middleware

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors")
 const importRoutes = require('./src/routes/import.routes')
 require('./src/jobs/jobWorker'); // start the job worker
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', importRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
